ab7e88b5108039ce41f1485a31e7503db26c75e1
Unknown <furkan.sarihan@stu.fsm.edu.tr>
2019-05-12 18:39:58 +0300

    VR Two camera viewport renderer
    two viewport render for eyes with renderbuffer objects. Displayed with imgui canvases. Also full screen renderer with GLFW

--- Engine/engine.cpp
+++ Engine/engine.cpp
@@ -2,9 +2,11 @@
 #include "editor.h"
 #include "server.h"
 
-#define screenWIDTH 1366
-#define screenHEIGTH 768
+#define screenWIDTH 1920
+#define screenHEIGTH 1080
 
+#define VRscreenRes 900
+#define VRscreenRes 900
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_press_callback(GLFWwindow* window, int button, int action, int mods);
@@ -30,6 +32,8 @@ const char* glsl_version = "#version 130";
 
 ImGuiWindowFlags window_flags;
 
+int VRoffsetX = (screenWIDTH/2 - VRscreenRes) / 2;
+int VRoffsetY = (screenHEIGTH - VRscreenRes) / 2;
 
 void webServer() {
 	server->startServer();
@@ -46,7 +50,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
 	//Creating window
-	GLFWwindow* window = glfwCreateWindow(screenWIDTH, screenHEIGTH, "Test program with GLFW", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(screenWIDTH, screenHEIGTH, "Test program with GLFW", glfwGetPrimaryMonitor(), NULL);
 
 	// Creation handling
 	if (window == nullptr) {
@@ -138,7 +142,7 @@ int main() {
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	
-	// Actual framebuffer operation
+	// Actual framebuffer operation 111111111111111111
 	unsigned int framebuffer;
 	glGenFramebuffers(1, &framebuffer);
 	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
@@ -155,13 +159,30 @@ int main() {
 	// attach it to currently bound framebuffer object
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
 
-	// Render buffer object
+	// Actual framebuffer operation 222222222222222222
+	unsigned int framebuffer2;
+	glGenFramebuffers(1, &framebuffer2);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer2);
+
+	// generate texture
+	unsigned int texColorBuffer2;
+	glGenTextures(1, &texColorBuffer2);
+	glBindTexture(GL_TEXTURE_2D, texColorBuffer2);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWIDTH, screenHEIGTH, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// attach it to currently bound framebuffer object
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer2, 0);
+
+	/*// Render buffer object
 	unsigned int rbo;
 	glGenRenderbuffers(1, &rbo);
 	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
 	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWIDTH, screenHEIGTH);
 
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);*/
 
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
 		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
@@ -178,10 +199,9 @@ int main() {
 	//window_flags |= ImGuiWindowFlags_NoDocking;
 
 	while (!glfwWindowShouldClose(window)) {
-		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		
 
-		glClearColor(0.8f, 0.8f, 0.8f, 0.4f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
@@ -195,15 +215,43 @@ int main() {
 		processInput(window, deltaTime, editor->scene.physicsWorld->dynamicsWorld);
 		editor->scene.physicsWorld->dynamicsWorld->stepSimulation(1 / 60.f, 10);
 		
+		//*****************************************************FRAME111111111111111111
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+		glClearColor(0.8f, 0.8f, 0.8f, 0.4f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		editor->renderObjects();
 		editor->renderGrid();
 
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
 		glDisable(GL_DEPTH_TEST);
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessery actually, since we won't be able to see behind the quad anyways)
 		glClear(GL_COLOR_BUFFER_BIT);
+		//*****************************************************FRAME111111111111111111
 
-		ImGui::Begin("Main ImGui", 0, window_flags);
+
+		//*****************************************************FRAME222222222222222222
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer2);
+
+		glClearColor(0.8f, 0.8f, 0.8f, 0.4f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		editor->renderObjects();
+		editor->renderGrid();
+
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+		glDisable(GL_DEPTH_TEST);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessery actually, since we won't be able to see behind the quad anyways)
+		glClear(GL_COLOR_BUFFER_BIT);
+		//*****************************************************FRAME22222222222222222222
+
+
+		// Imgui for editor scene
+		/*ImGui::Begin("Main ImGui", 0, window_flags);
 		ImGui::SetWindowPos(ImVec2(0, 0));
 		ImGui::SetWindowSize(ImVec2(screenWIDTH, screenHEIGTH));
 		ImGui::End();
@@ -212,12 +260,35 @@ int main() {
 
 		ImGui::Begin("Scene Window");
 		ImGui::GetWindowDrawList()->AddImage(
+			(void *)texColorBuffer2,
+			ImVec2(ImGui::GetCursorScreenPos()),
+			ImVec2(ImGui::GetCursorScreenPos().x + 860,
+				ImGui::GetCursorScreenPos().y + 860),
+			ImVec2(0, 1), ImVec2(1, 0));
+		ImGui::SetWindowSize(ImVec2(860, 860));
+		ImGui::End();*/
+
+		// Imgui for RemoteVR operation
+		ImGui::Begin("Left Eye");
+		ImGui::GetWindowDrawList()->AddImage(
 			(void *)texColorBuffer,
 			ImVec2(ImGui::GetCursorScreenPos()),
-			ImVec2(ImGui::GetCursorScreenPos().x + screenWIDTH / 2,
-				ImGui::GetCursorScreenPos().y + screenHEIGTH / 2),
+			ImVec2(ImGui::GetCursorScreenPos().x + VRscreenRes,
+				ImGui::GetCursorScreenPos().y + VRscreenRes),
 			ImVec2(0, 1), ImVec2(1, 0));
-		ImGui::SetWindowSize(ImVec2(screenWIDTH/2, screenHEIGTH/2));
+		ImGui::SetWindowSize(ImVec2(VRscreenRes, VRscreenRes));
+		ImGui::SetWindowPos(ImVec2(VRoffsetX, VRoffsetY));
+		ImGui::End();
+
+		ImGui::Begin("Right Eye");
+		ImGui::GetWindowDrawList()->AddImage(
+			(void *)texColorBuffer2,
+			ImVec2(ImGui::GetCursorScreenPos()),
+			ImVec2(ImGui::GetCursorScreenPos().x + VRscreenRes,
+				ImGui::GetCursorScreenPos().y + VRscreenRes),
+			ImVec2(0, 1), ImVec2(1, 0));
+		ImGui::SetWindowSize(ImVec2(VRscreenRes, VRscreenRes));
+		ImGui::SetWindowPos(ImVec2(screenWIDTH / 2 + VRoffsetX, VRoffsetY));
 		ImGui::End();
 
 		// Swap buffers and check for events and editor
@@ -237,7 +308,7 @@ int main() {
 	editor->deletePhysics();
 	
 	// join webserver thread
-	//first.join();
+	first.join();
 	terminate();
 
 	return EXIT_SUCCESS;
